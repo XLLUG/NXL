f0e0bdbb30efb37d7f96e9e7d27e1afb828a5005
罗佳 <18700870001@163.com>
2018-08-15 22:37:18 +0800

    didi主页面

--- didi/js/user.js
+++ didi/js/user.js
@@ -1,9 +1,44 @@
 $(function () {
     /*当点击用户头像时*/
+    var a = 1;
     $(".user").click(function () {
-        $(".setting").fadeIn("slow");
-    })
+
+        if(a){
+            $(".setting").fadeIn("slow");
+            $(".user_setting").css("transform","translateX(0)");
+            a = 0;
+        }else{
+            $(".login").fadeIn();
+        }
+
+    });
     $(".mask").click(function () {
-        $(".setting").fadeOut("slow");
+        $(".setting").fadeOut("fast");
+        $(".user_setting").css("transform","translateX(-70%)");
+
+    });
+    /*当点击信息时*/
+    $(".message").click(function () {
+        $(".message_detail").css("display","block");
+        $(".message_detail").css("transform","translateX(0)");
+    });
+    $(".message_back").click(function () {
+        $(".message_detail").css("display","none");
+        $(".message_detail").css("transform","translateX(100%)");
+    })
+    $(".login_back").click(function () {
+        $(".login").fadeOut();
+    })
+    $(".login_next").click(function () {
+        $(".login_password").fadeIn();
+        $(".login").fadeOut();
+    })
+    $(".login_sure").click(function () {
+        $(".login_password").fadeOut();
+        a=1;
+    })
+    $(".pass_back").click(function () {
+        $(".login_password").fadeOut();
+        $(".login").fadeIn();
     })
 });
